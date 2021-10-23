8953b4ed69ef79ce11e1c75cf07daef36f028a66
b-s-ad <ahmedshehbazb@gmail.com>
2021-10-23 11:39:18 -0700

    UC2

--- Tic/TicTacToeGame.cs
+++ Tic/TicTacToeGame.cs
@@ -2,14 +2,44 @@
 
 namespace TicTacToeGame
 {
-    class TicTacToeGame
+    public class TicTacToeGame
     {
-        
-        static void Main(string[] args)
-        {
-            char[] board = new char[10];
 
+        public static void Main(string[] args)
+        {
+            char[] board = createboard();
+            
+        }
+
+        //uc1
+        private static char[] createboard() { 
+            char[] board = new char[10];
+            for (int i = 0; i < 10; i++){
+                board[i] = ' ';
 
          }
+    return board;
+
     }
+        //uc2
+        public static void chooselettter()
+
+            
+        {
+            Console.WriteLine("choose the letter X or O");
+            
+            string choose;
+            choose = Console.ReadLine();              
+           switch(choose)
+            {
+                case "O":
+                    Console.WriteLine("User is using X");
+                    break;
+                case "X":
+                    Console.WriteLine("computer is using O");
+                    break;
+
+            }
+        }
+}
 }
