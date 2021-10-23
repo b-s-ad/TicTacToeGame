86571c4b594d9a3ee31a4243f8f9477502b7e3b5
b-s-ad <ahmedshehbazb@gmail.com>
2021-10-23 11:57:44 -0700

    uc3

--- Tic/TicTacToeGame.cs
+++ Tic/TicTacToeGame.cs
@@ -1,15 +1,59 @@
 ﻿using System;
 
-namespace TicTacToeGame 
+namespace TicTacToeGame
 {
-    class TicTacToeGame
+    public class TicTacToeGame
     {
-        
-        static void Main(string[] args)
-        {
-            char[] board = new char[10];
 
+        public static void Main(string[] args)
+        {
+            char[]board = createboard();
+            
+        }
+
+        //uc1
+        private static char[] createboard() { 
+            char[]board = new char[10];
+            for (int i = 0; i < 10; i++){
+                board[i] = ' ';
 
          }
+    return board;
+
     }
+        //uc2
+        public static void chooselettter()
+
+            
+        {
+            Console.WriteLine("choose the letter X or O");
+            
+            string choose;
+            choose = Console.ReadLine();              
+           switch(choose)
+            {
+                case "O":
+                    Console.WriteLine("User is using X");
+                    break;
+                case "X":
+                    Console.WriteLine("computer is using O");
+                    break;
+
+            }
+        }
+        //uc3
+
+        private static void showboard(char[]board)
+
+        {
+            Console.WriteLine("show the current board");
+            Console.WriteLine("board[1]\t" + "board[2]\t" + "board[3]\t");
+            Console.WriteLine("\n");
+            Console.WriteLine("board[4]\t" + "board[5]\t" + "board[6]\t");
+            Console.WriteLine("\n");
+            Console.WriteLine("board[7]\t" + "board[8]\t" + "board[9]\t");
+            Console.WriteLine("\n");
+
+        }
+}
 }
